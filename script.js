ebdb2f7874cb102fb17038f31824e7e315925d43
Wynne <76490156+wwyynnee@users.noreply.github.com>
2022-11-27 08:58:05 +0000

    Разработка меню, окончательная разработка сайта

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 $("#up").click(() => {
   $("html, body").animate({ scrollTop: 0}, 0);
 });
+
+// Меню
+$(".menu").hide();
+$("*").on("click", (e) => {
+  if ($(e.target).is(".menu, .menu ul, .menu svg, .menu svg path, .nav__container")) {
+    return false;
+  }
+
+  $(".menu").hide();
+})
+const menu = () => {
+  $(".menu").toggle();
+}
